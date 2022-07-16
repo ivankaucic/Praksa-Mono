19f3649cdbda95f4f80b63b4f0912a5397362193
Ivan Kaucic <ivan.kaucic01@gmail.com>
2022-07-16 10:53:20 +0200

    gotov prvi zadatak s frontenda

--- FE/index.js
+++ FE/index.js
@@ -1,1 +1,18 @@
-console.log("Jebem ti mater")
+function addBook(){
+    var table = document.getElementById("myTable");
+    var row = table.insertRow();
+    var cellId = row.insertCell();
+    var cellBookName = row.insertCell();
+    var cellBookAuthor = row.insertCell();
+    var cellBookPrice = row.insertCell();
+
+    var bookId = document.getElementById("bookId").value;
+    var bookName = document.getElementById("bookName").value;
+    var bookAuthor = document.getElementById("bookAuthor").value;
+    var bookPrice = document.getElementById("bookPrice").value;
+
+    cellId.innerHTML = bookId;
+    cellBookName.innerHTML = bookName;
+    cellBookAuthor.innerHTML = bookAuthor;
+    cellBookPrice.innerHTML = bookPrice;
+}
